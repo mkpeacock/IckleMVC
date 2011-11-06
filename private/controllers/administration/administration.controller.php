0fed997f03e92ed724060b823feb783d2778290f
Michael Peacock <mkpeacock@gmail.com>
2011-11-06 20:11:47 +0000

    check user is logged in and is an administrator

--- private/controllers/administration/administration.controller.php
+++ private/controllers/administration/administration.controller.php
@@ -6,15 +6,30 @@
  */
 class AdministrationController{
 	
+	/**
+	 * Registry object
+	 * @var IckleRegistry
+	 */
 	private $registry;
+	
+	/**
+	 * Valid administration sections
+	 * @var array
+	 */
 	private $sections = array( 'dashboard', 'content', 'users' );
 	
+	/**
+	 * Constructor
+	 * @param IckleRegistry $registry
+	 * @param bool $autoProcess
+	 * @return void
+	 */
 	public function __construct( IckleRegistry $registry, $autoProcess=true )
 	{
 		$this->registry = $registry;
 		if( $this->registry->getObject('authentication')->isLoggedIn() )
 		{
-			if( $this->registry->getObject('authentication')->getUser()->getAdministrator() )
+			if( $this->registry->getObject('authentication')->getUser()->isAdministrator() )
 			{
 				$this->process();
 			}
@@ -29,19 +44,32 @@ class AdministrationController{
 		}
 	}
 	
+	/**
+	 * Request the user logs in
+	 * @return void
+	 */
 	private function requestLogin()
 	{
 		$this->registry->getObject('template')->buildFromTemplates( 'login.tpl.php' );
 	}
 	
+	/**
+	 * Request the user logs in as an administrator
+	 * @return void
+	 */
 	private function notAdministrator()
 	{
-		
+		$this->registry->getObject('template')->buildFromTemplates( 'login.tpl.php' );
+		$this->registry->notify('error', 'Access denied', 'Sorry, only administrators have permission to access the administration area');
 	}
 	
+	/**
+	 * User is logged in and is an administrator: process their request
+	 * @return void
+	 */
 	private function process()
 	{
-		
+		echo 'success';
 	}
 	
 	
