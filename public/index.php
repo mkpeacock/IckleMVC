a5186b738fe0dced40b2b99da463a69b849f12a1
Michael Peacock <mkpeacock@gmail.com>
2011-11-18 17:37:47 +0000

    removed old comment code from the bottom of index.php

--- public/index.php
+++ public/index.php
@@ -9,16 +9,6 @@ define('ACCESS_POINT', 'FRONT' );
 define( "PUBLIC_PATH", dirname( __FILE__ ) ."/" );
 require_once('../private/bootstrap.php');
 $bootstrap = new Bootstrap();
-//$bootstrap->processRequest();
-//require_once( FRAMEWORK_PATH . 'models/content/content.php' );
-//require_once( FRAMEWORK_PATH . 'models/content/page.php' );
-//$page = new Pagecontent( $bootstrap->getRegistry(), 0, 'test-page' );
-//require_once( FRAMEWORK_PATH . 'models/ecommerce/purchasable.php' );
-//require_once( FRAMEWORK_PATH . 'models/ecommerce/product.php' );
-//require_once( FRAMEWORK_PATH . 'models/ecommerce/purchasable.php' );
-//$product = new Product( $bootstrap->getRegistry(), 1 );
-//echo $content->generateSQL();
-//print $bootstrap->getRegistry()->getObject('template')->generateOutput()->getPage()->getContent();
 
 
 ?>
