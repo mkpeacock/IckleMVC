55cbf45b96e5333da01efbfe65773f2f8312dd17
Michael Peacock <mkpeacock@gmail.com>
2011-11-25 19:38:02 +0000

    Updated the Data_Model to inherit from the Data_Save abstract.  Has a basic save method and buildFromSQL method.

--- private/IckleMVC/Models/Data/Model.php
+++ private/IckleMVC/Models/Data/Model.php
@@ -4,7 +4,7 @@ namespace IckleMVC\Models;
  * Abstract model class
  * @author Michael Peacock
  */
-abstract class Data_Model{
+abstract class Data_Model extends Data_Save{
 	
 	/**
 	 * Indicates if the object is a valid representation, so for database backed objects, if the record was found
@@ -17,6 +17,9 @@ abstract class Data_Model{
 	 * @var array
 	 */
 	protected $data = array();
+	
+	protected $primaryKeyField = null;
+	protected $table;
 		
 	/**
 	 * Constructor
@@ -26,11 +29,60 @@ abstract class Data_Model{
 	 */
 	abstract public function __construct( \IckleMVC\Registry\IckleRegistry $registry, $ID=0 );
 	
+	protected function setPrimaryKeyField( $pkf='id' )
+	{
+		$this->primaryKeyField = $pkf;
+	}
+	
+	protected function setTable( $table='' )
+	{
+		$this->table = $table;
+	}
+	
+	protected function buildFromQuery( $sql )
+	{
+		$this->registry->getObject('db')->executeQuery( $sql );
+		if( $this->registry->getObject('db')->getNumRows() == 1 )
+		{
+			$this->valid = true;
+			$row = $this->registry->getObject('db')->getRows();
+			foreach( $row as $field => $data )
+			{
+				$this->fieldNameToProperty( $field );
+				$this->$field = $data;
+			}
+		}
+	}
+	
 	/**
 	 * Save the model 
 	 * @return void
 	 */
-	abstract public function save();
+	public function save()
+	{
+		$pkf = $this->primaryKeyField;
+		$this->resanitize();
+		
+		if( null != $pkf && intval( $this->$pkf ) > 0 )
+		{
+			// update
+			if( is_numeric( $this->$pkf ) && $this->$pkf === intval( $this->$pkf ) )
+			{
+				// its an int
+				$this->registry->getObject('db')->updateRecords( $this->table, $this->data, "{$this->primaryKeyField}={$this->$pkf}" );
+			}
+			else
+			{
+				// its a string
+				$this->registry->getObject('db')->updateRecords( $this->table, $this->data, "{$this->primaryKeyField}='{$this->$pkf}'" );
+			}
+		}
+		else
+		{
+			// insert
+			$this->registry->getObject('db')->insertRecords( $this->table, $this->data );
+		}
+	}
 	
 	/**
 	 * Returns if the model is valid or not
@@ -49,6 +101,16 @@ abstract class Data_Model{
 	public function setValid( $valid )
 	{
 		$this->valid = $valid;
+	}
+	
+	/**
+	 * Overloading isset
+	 * @param String $name the name of the field
+	 * @return bool
+	 */
+	public function __isset( $name )
+	{
+		return isset( $this->data[ $name ] );
 	}
 	
 	/**
@@ -85,9 +147,9 @@ abstract class Data_Model{
 	public function fieldNameToProperty( $name )
 	{
 		$property = implode( ( array_map( 'ucfirst', explode( '_', $name ) ) ) );
-		return lcfirst( $property );
+		return lcfirst( $property );	
 	}
-	
+		
 	/**
 	 * Converts a property name into a field name
 	 * @param String $property the name of the object property e.g. id, name, someProperty
