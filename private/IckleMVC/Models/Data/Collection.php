0d5e62c0eff68385ab6b4231fa095e5652f0d3d1
Michael Peacock <mkpeacock@gmail.com>
2011-11-25 18:41:30 +0000

    Abstract Data_Collection class for holding a collection of objects.  The Data package is for objects which are primarilly "data-access" objects

---
+++ private/IckleMVC/Models/Data/Collection.php
@@ -0,0 +1,31 @@
+<?php
+namespace IckleMVC\Models;
+
+abstract class Data_Collection implements IteratorAggregate, Countable{
+	
+	protected $objects = array();
+
+    public function __construct( \IckleMVC\Registry\IckleRegistry $registry )
+    {
+    	$this->registry = $registry;
+    }
+    
+    public function getIterator()
+    {
+    	return new \ArrayIterator( $this->objects );
+    }
+    
+    public function count()
+    {
+    	return count( $this->objects );
+    }
+    
+    public function add( $object )
+    {
+    	$this->objects[] = $object;
+    }
+    
+    abstract protected function buildFromSQL( $sql );
+    
+}
+?>
