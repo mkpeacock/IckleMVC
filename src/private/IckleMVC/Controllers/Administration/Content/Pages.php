60c30985660a123e270643c3a8f11fff22f87da8
Michael Peacock <mkpeacock@gmail.com>
2011-11-26 17:37:41 +0000

    Pages (admin) controller set to load the view

--- src/private/IckleMVC/Controllers/Administration/Content/Pages.php
+++ src/private/IckleMVC/Controllers/Administration/Content/Pages.php
@@ -7,12 +7,13 @@ class Administration_Content_Pages extends Controller {
 	public function __construct( \IckleMVC\Registry\IckleRegistry $registry, $autoProcess=true )
 	{
 		$this->registry = $registry;
+		$this->listPages();
 	}
 	
 	
 	public function listPages()
 	{
-		
+		$this->loadView( 'administration_content_pages_list' )->generate();
 	}
 	
 	
