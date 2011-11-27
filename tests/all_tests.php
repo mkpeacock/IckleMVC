82ce53d47f550b8078b1aeb81c886505ecdaf373
Michael Peacock <mkpeacock@gmail.com>
2011-11-27 12:36:59 +0000

    Moved namespace loading from database_test to all_tests

--- tests/all_tests.php
+++ tests/all_tests.php
@@ -1,4 +1,32 @@
 <?php
+
+define( "FRAMEWORK_PATH", dirname( __FILE__ ) ."/../src/private/" );
+
+require_once( FRAMEWORK_PATH . 'splClassLoader.php' );
+$classLoader = new SplClassLoader('IckleMVC\Registry', FRAMEWORK_PATH );
+$classLoader->register();
+$classLoader = new SplClassLoader('IckleMVC\Models', FRAMEWORK_PATH );
+$classLoader->register();
+$classLoader = new SplClassLoader('IckleMVC\Controllers', FRAMEWORK_PATH );
+$classLoader->register();
+$classLoader = new SplClassLoader('IckleMVC\Libraries', FRAMEWORK_PATH );
+$classLoader->register();
+$classLoader = new SplClassLoader('IckleMVC\Views', FRAMEWORK_PATH );
+$classLoader->register();
+
+$defaultRegistryObjects = array(
+									'db' => '\IckleMVC\Registry\Database_MySQL',
+									'template' => '\IckleMVC\Registry\Template',
+									'scope' => '\IckleMVC\Registry\PrimaryScope',
+									'urlprocessor' => '\IckleMVC\Registry\URLProcessor',
+									'frontmenu' => '\IckleMVC\Registry\FrontMenu',
+									'contenttreebuilder' => '\IckleMVC\Registry\ContentTreeBuilder',
+									'authentication' => '\IckleMVC\Registry\Authentication'
+									
+
+								);
+								
+								
 require_once('test_constants.php');
 require_once('../../simpletest/autorun.php');
 define( "TESTS_PATH", dirname( __FILE__ ) ."/" );
--- tests/database_test.php
+++ tests/database_test.php
@@ -1,31 +1,5 @@
 <?php
-
-define( "FRAMEWORK_PATH", dirname( __FILE__ ) ."/../src/private/" );
-
-require_once( FRAMEWORK_PATH . 'splClassLoader.php' );
-$classLoader = new SplClassLoader('IckleMVC\Registry', FRAMEWORK_PATH );
-$classLoader->register();
-$classLoader = new SplClassLoader('IckleMVC\Models', FRAMEWORK_PATH );
-$classLoader->register();
-$classLoader = new SplClassLoader('IckleMVC\Controllers', FRAMEWORK_PATH );
-$classLoader->register();
-$classLoader = new SplClassLoader('IckleMVC\Libraries', FRAMEWORK_PATH );
-$classLoader->register();
-$classLoader = new SplClassLoader('IckleMVC\Views', FRAMEWORK_PATH );
-$classLoader->register();
-
-$defaultRegistryObjects = array(
-									'db' => '\IckleMVC\Registry\Database_MySQL',
-									'template' => '\IckleMVC\Registry\Template',
-									'scope' => '\IckleMVC\Registry\PrimaryScope',
-									'urlprocessor' => '\IckleMVC\Registry\URLProcessor',
-									'frontmenu' => '\IckleMVC\Registry\FrontMenu',
-									'contenttreebuilder' => '\IckleMVC\Registry\ContentTreeBuilder',
-									'authentication' => '\IckleMVC\Registry\Authentication'
-									
-
-								);
-										
+						
 
 Mock::generate('IckleMVC\Registry\IckleRegistry', 'MockIckleRegistry');
 
@@ -58,21 +32,36 @@ class TestOfDatabase extends UnitTestCase {
     
     public function testInsert()
     {
+    	$this->database->newConnection( default_host, default_user, default_password, default_database );
     	$insert = array( 'some_field' => 'my_value' );
-    	$this->database->insertRecords( 'test_dba', $insert );
+    	$this->database->insertRecord( 'test_dba', $insert );
     	$result = $this->mysqli->query("SELECT * FROM test_dba");
     	if( $result->num_rows == 1 )
     	{
-    		//$row = $result->fetch_row();
-    		//echo '<pre>' . print_r( $row, true ) . '</pre>';
+    		$row = $result->fetch_assoc();
+    		if( $row['id'] == 1 && $row['some_field'] == 'my_value' )
+    		{
+    			$this->pass();
+    		}
+    		else
+    		{
+    			$this->fail( "Table contents don't match what was inserted" );
+    		}
     	}
     	else
     	{
     		$this->fail( "No records inserted" );
-    	}
+    	}	
+    	    	
+    }
+    
+    public function testInsertWithKnownDuplicatePK()
+    {
     	
-    	
-    	
+    }
+    
+    public function testUpdate()
+    {
     	
     }
     
