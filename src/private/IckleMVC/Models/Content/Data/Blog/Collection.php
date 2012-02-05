d1649f33f549407b37cb1bc8b845ed64d35fdb44
Michael Peacock <mkpeacock@gmail.com>
2012-02-05 21:56:07 +0000

    Blog collection which links to pagination

--- src/private/IckleMVC/Models/Content/Data/Blog/Collection.php
+++ src/private/IckleMVC/Models/Content/Data/Blog/Collection.php
@@ -7,6 +7,23 @@ class Content_Data_Blog_Collection extends Content_Data_Contents{
 		$this->registry = $registry;
 	}
 	
+	public function buildPagination( $page=0 )
+	{
+		$p = new IckleMVC\Libraries\Pagination_Generator( $this->registry->getObject('db') );
+		$p->setLimit(10);
+		$p->setOffset( $page );
+		$p->setQuery( $this->getSQL() );
+		$p->generatePagination();
+		$this->pagination = $p;
+	}
+	
+	public function buildFromPagination()
+	{
+		$this->buildFromArray( $this->pagination->getResults(), new Content_Data_Blog_Model( $this->registry ) ); 
+	}
+	
+	
+	
 	public function getSQL( $conditions='' )
 	{
 		$curtime = date('Y-m-d H:i:s');
