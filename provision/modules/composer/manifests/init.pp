f2cbe09753c20ddcc24e25f8e1447aab3dd273d5
Michael Peacock <mkpeacock@gmail.com>
2012-05-13 18:13:01 +0100

    We need to provide the path to rm in order to actually delete the composer related stuff.

--- provision/modules/composer/manifests/init.pp
+++ provision/modules/composer/manifests/init.pp
@@ -10,7 +10,9 @@ class composer {
 		require => Exec['apt-get update']
 	}
 	
-	exec{ "compose"
-		command => 'rm -rfv /vagrant/vendor/* && rm -f /vagrant/composer.lock && /usr/bin/curl -s http://getcomposer.org/installer | /usr/bin/php -- --install-dir=/vagrant && cd /vagrant && /usr/bin/php /vagrant/composer.phar install',
+	exec{ "compose":
+		command => '/bin/rm -rfv /vagrant/vendor/* && /bin/rm -f /vagrant/composer.lock && /usr/bin/curl -s http://getcomposer.org/installer | /usr/bin/php -- --install-dir=/vagrant && cd /vagrant && /usr/bin/php /vagrant/composer.phar install',
 		require => [ Package['curl'], Package['git-core'] ]
 	}
+	
+}
