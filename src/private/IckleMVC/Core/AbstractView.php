88a2738db8247d7cef26df73a0ff379b3c2412bc
Michael Peacock <mkpeacock@gmail.com>
2012-05-19 19:40:16 +0100

    Moving and cleaning abstractview

--- src/private/IckleMVC/Core/AbstractView.php
+++ src/private/IckleMVC/Core/AbstractView.php
@@ -1,13 +1,17 @@
 <?php
-namespace IckleMVC\Views;
+namespace IckleMVC\Core;
 abstract class AbstractView {
 	
 	protected $registry;
+	protected $templateEngine;
+	protected $frontMenu;
 	private $applicationView=null;
 	
 	public function __construct( \IckleMVC\Registry\IckleRegistry $registry, $applicationView=null)
 	{
 		$this->registry = $registry;
+		$this->templateEngine = $this->registry->getObject('template');
+		$this->frontMenu = $this->registry->getObject('frontmenu');
 		$this->applicationView = $applicationView;
 	}
 	
@@ -20,8 +24,8 @@ abstract class AbstractView {
 		{
 			$applicationView->preParseHook();
 		}
-		$this->registry->getObject('template')->parseOutput();
-		print $this->registry->getObject('template')->getPage()->getContentToPrint();
+		$this->templateEngine();
+		print $this->templateEngine->getPage()->getContentToPrint();
 		exit();
 	}
 	
